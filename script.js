76740ca8bb861ef69d32e45335b69231db939cec
Adwithr <adwithr8277@gmail.com>
2024-02-27 19:47:26 +0530

    added final answer code

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
 let buttons = Array.from(document.querySelectorAll(".buttons"));
 let uprDisplay = document.querySelector(".uprDisplay");
 let lwrDisplay = document.querySelector(".lwrDisplay");
-let firstVal,
-  secVal = null;
-let oper;
+let firstVal, secVal, oper;
 
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
     if (e.target.textContent === "C") clearDisplay();
     else if (e.target.classList.contains("oper")) {
       oper = e.target.textContent;
-      firstVal = lwrDisplay.textContent;
-      uprDisplay.textContent = firstVal + e.target.textContent;
+      firstVal = +lwrDisplay.textContent;
+      uprDisplay.textContent += firstVal + e.target.textContent;
       lwrDisplay.textContent = "";
-    } else if (e.target.textContent === "=") {
-      let res = operate(firstVal, secVal, oper);
-    } else lwrDisplay.textContent += e.target.textContent;
+    } else if (e.target.textContent === "=") calc();
+    else if (e.target.classList.contains("backspace"))
+      lwrDisplay.textContent = lwrDisplay.textContent.slice(0, -1);
+    else lwrDisplay.textContent += e.target.textContent;
   });
 });
 
 function clearDisplay() {
   uprDisplay.textContent = "";
   lwrDisplay.textContent = "";
+  firstVal = 0;
+  secVal = 0;
+  oper = "";
 }
 
 function operate(firstVal, secVal, oper) {
@@ -33,7 +35,14 @@ function operate(firstVal, secVal, oper) {
     case "*":
       return firstVal * secVal;
     case "/":
-      if (secVal == 0) return "ERROR";
+      if (secVal == 0) return "oooof";
       else return firstVal / secVal;
   }
+}
+
+function calc() {
+  secVal = +lwrDisplay.textContent;
+  uprDisplay.textContent += secVal;
+  let res = operate(firstVal, secVal, oper);
+  lwrDisplay.textContent = `= ${res}`;
 }
